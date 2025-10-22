71ca8555aeb5cb5c25e852157d77309118ce0ed0
HIDEAKI_KAGAMI <syumeikyo@outlook.jp>
2025-10-22 17:24:09 +0900

    chore(web): vitest.setup.tsの環境変数名不正対応

--- apps/web/vitest.setup.ts
+++ apps/web/vitest.setup.ts
@@ -8,7 +8,7 @@ afterEach(() => {
 })
 
 // Next.js環境変数のモック
-process.env.NEXT_PUBLIC_BASE_URL = 'http://localhost:3000';
+process.env.NEXT_PUBLIC_SITE_URL = 'http://localhost:3000';
 
 // ResizeObserverのモック
 global.ResizeObserver = vi.fn().mockImplementation(() => ({
